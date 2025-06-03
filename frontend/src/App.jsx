f3931d36eea10d455f8610a2ca8e615e639d97e3
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-06-03 19:36:57 -0400

    updated frontend setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,20 @@
-import React from "react";
+import { Outlet } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Navigation from "./pages/Auth/Navigation";
 
 function App() {
   return (
-    <div className="flex items-center h-screen bg-gray-800 text-2xl text-white font-bold justify-center">
-      App
-    </div>
+    // <div className="flex items-center h-screen bg-gray-800 text-2xl text-white font-bold justify-center">
+    //   App
+    // </div>
+    <>
+      <ToastContainer />
+      <Navigation />
+      <main className="py-3">
+        <Outlet />
+      </main>
+    </>
   );
 }
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import store from "./redux/store.js";
+import { Provider } from "react-redux";
+import { Route, RouterProvider, createRoutesFromElements } from "react-router";
+import { createBrowserRouter } from "react-router-dom";
+import ErrorPage from "./pages/ErrorPage.jsx";
+import Home from "./pages/Home.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// Auth
+
+//Restricted
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />} errorElement={<ErrorPage />}>
+      <Route index={true} path="/" element={<Home />} />
+    </Route>
+  )
+);
+
+createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
+);
