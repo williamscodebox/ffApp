1e1f1b8f90e89bcf5d5db2e7373aa11e21881378
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-06-03 18:10:44 -0400

    first commit

---
+++ backend/controllers/userController.js
@@ -0,0 +1,42 @@
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import asyncHandler from "../middlewares/asyncHandler.js";
+import createToken from "../utils/createToken.js";
+
+const createUser = asyncHandler(async (req, res) => {
+  const { username, email, password } = req.body;
+
+  if (!username || !email || !password) {
+    throw new Error("Please fill all the fields");
+  }
+
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  // Hash the user passwword
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const newUser = new User({ username, email, password: hashedPassword });
+
+  try {
+    await newUser.save();
+    createToken(res, newUser._id);
+
+    res.status(201).json({
+      _id: newUser._id,
+      username: newUser.username,
+      email: newUser.email,
+      isAdmin: newUser.isAdmin,
+    });
+  } catch (error) {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
+});
+
+export { createUser };
---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="flex items-center h-screen bg-gray-800 text-2xl text-white font-bold justify-center">
+      App
+    </div>
+  );
+}
+
+export default App;
