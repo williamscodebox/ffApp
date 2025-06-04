b89c52699c7d5ae2eab9945fbe825e020c5f5b55
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-06-04 00:06:02 -0400

    added InformationCard

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import InformationCard from "../components/InformationCard";
 
 function Home() {
-  return <div className="mt-[5rem]">Home</div>;
+  return (
+    <div className="mt-10 home-bg">
+      <div>
+        <InformationCard />
+      </div>
+    </div>
+  );
 }
 
 export default Home;
