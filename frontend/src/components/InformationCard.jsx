4f4923463506902e215e51adf42f9ef83df816de
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-06-04 00:57:36 -0400

    added WelcomeCard

--- frontend/src/components/InformationCard.jsx
+++ frontend/src/components/InformationCard.jsx
@@ -2,11 +2,24 @@ import React from "react";
 
 const InformationCard = () => {
   return (
-    <div className="relative top-[5rem] left-[8rem] w-[80%] h-[25rem] bg-gray-800 p-6 pl-30 border-1 border-gray-300 rounded-2xl z-40 shadow-2xl text-2xl text-white font-bold opacity-90">
-      <h1>Welcome to the 2025 NFL Regular season Football Pool</h1>
+    <div className="relative top-[5rem] left-[8rem] w-[80%] h-[25rem] bg-gray-800 p-6 pl-30 border-1 border-gray-300 rounded-2xl z-40 shadow-2xl text-xl text-white font-normal opacity-90">
+      <h1 className="text-2xl font-bold">
+        Welcome to the 2025 NFL Regular season Football Pool
+      </h1>
       <p className="text-xl text-white font-normal">
-        Brought to you by Canadian Tire Store 092
+        Brought to you by CodeSrev Entertainment
       </p>
+      <p className="mt-6 text-2xl font-bold">What to expect:</p>
+      <p className="mt-2">18 Weeks, 32 Teams</p>
+      <p>Picks to be Completed every week by Tuesday Evening</p>
+      <p>Each game pick only the team you choose will win</p>
+      <p>
+        The final game of each week pick your team as well as include scores
+      </p>
+      <p>50% of proceeds to be donated to local foodbank</p>
+      <p>Winner will receive the other 50% as the prize pot</p>
+      <p>Good luck to all</p>
+      <p>Have fun and enjoy the football season</p>
     </div>
   );
 };
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,31 @@
-import React from "react";
+import { motion } from "motion/react";
 import InformationCard from "../components/InformationCard";
+import WelcomeCard from "../components/WelcomeCard";
 
 function Home() {
   return (
     <div className="mt-10 home-bg">
       <div>
-        <InformationCard />
+        <motion.div
+          // className="box"
+          initial={{ x: -1300 }}
+          animate={{ x: 0 }}
+          transition={{ delay: 0.1, ease: "easeOut", duration: 1.75 }}
+        >
+          <InformationCard />
+        </motion.div>
       </div>
+      {/* <motion.div
+        initial={{ opacity: 0, scale: 0.5 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{
+          duration: 0.8,
+          delay: 0.5,
+          ease: [0, 0.71, 0.2, 1.01],
+        }}
+      >
+        <WelcomeCard />
+      </motion.div> */}
     </div>
   );
 }
