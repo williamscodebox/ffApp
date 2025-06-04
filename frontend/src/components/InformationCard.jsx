f1ace8b1d7edb162111d128d7dada84ec06c4b76
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-06-04 00:14:16 -0400

    added framer motion package

--- frontend/src/components/InformationCard.jsx
+++ frontend/src/components/InformationCard.jsx
@@ -3,7 +3,10 @@ import React from "react";
 const InformationCard = () => {
   return (
     <div className="relative top-[5rem] left-[8rem] w-[80%] h-[25rem] bg-gray-800 p-6 pl-30 border-1 border-gray-300 rounded-2xl z-40 shadow-2xl text-2xl text-white font-bold opacity-90">
-      Welcome to the 2025 NFL Regular season Football Pool
+      <h1>Welcome to the 2025 NFL Regular season Football Pool</h1>
+      <p className="text-xl text-white font-normal">
+        Brought to you by Canadian Tire Store 092
+      </p>
     </div>
   );
 };
