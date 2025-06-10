cf7eba6c2b9c182bba30e87193cae0f26992c487
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-06-10 15:09:04 -0400

    added sidebar

--- frontend/src/pages/Auth/Menu.jsx
+++ frontend/src/pages/Auth/Menu.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Sidebar from "../../components/Sidebar";
 
 const Menu = () => {
-  return <div className="mt-20">Menu</div>;
+  return (
+    <div className="mt-20">
+      <div>Current Standings</div>
+      <div>
+        <Sidebar />
+      </div>
+    </div>
+  );
 };
 
 export default Menu;
