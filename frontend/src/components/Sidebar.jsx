6eecf9a86b045459d6e770e541b5672096d7ff57
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-06-12 19:07:21 -0400

    updated Sidebar

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -12,8 +12,25 @@ function Sidebar() {
     updateValues(update);
   };
 
+  const pattern = [
+    [0, 0],
+    [1, 0],
+    [2, 0],
+    [3, 0],
+    [4, 1],
+    [5, 1],
+    [6, 1],
+    [7, 1],
+    [8, 1],
+    [9, 2],
+    [10, 2],
+    [11, 2],
+    [12, 2],
+  ];
+
   return (
-    <div className="bg-gray-900 text-white w-[10rem] h-full p-4 pt-10 fixed">
+    <div className="bg-gray-900 text-white w-[10rem] h-full p-4 pt-10 ">
+      {/*add fixed above to the div css and then add scroll bar to sidebar*/}
       <h1 className="text-xl">Dashboard</h1>
       <ul className="mt-6 space-y-4">
         <li>
@@ -25,87 +42,20 @@ function Sidebar() {
             <span className="hidden sm:block">Home</span>
           </Link>
         </li>
-        <li>
-          <Link
-            to="/Week"
-            className="flex items-center text-white transition-all duration-300 hover:translate-x-2"
-            onClick={() => handleClick(0, 0)}
-          >
-            <span className="hidden sm:block">Week 1</span>
-          </Link>
-        </li>
-        <li>
-          <Link
-            to="/Week"
-            className="flex items-center text-white transition-all duration-300 hover:translate-x-2"
-            onClick={() => handleClick(1, 0)}
-          >
-            <span className="hidden sm:block">Week 2</span>
-          </Link>
-        </li>
-        <li>
-          <Link
-            to="/Week"
-            className="flex items-center text-white transition-all duration-300 hover:translate-x-2"
-            onClick={() => handleClick(2, 0)}
-          >
-            <span className="hidden sm:block">Week 3</span>
-          </Link>
-        </li>
-        <li>
-          <Link
-            to="/Week"
-            className="flex items-center text-white transition-all duration-300 hover:translate-x-2"
-            onClick={() => handleClick(3, 0)}
-          >
-            <span className="hidden sm:block">Week 4</span>
-          </Link>
-        </li>
-        <li>
-          <Link
-            to="/Week"
-            className="flex items-center text-white transition-all duration-300 hover:translate-x-2"
-            onClick={() => handleClick(4, 1)}
-          >
-            <span className="hidden sm:block">Week 5</span>
-          </Link>
-        </li>
-        <li>
-          <Link
-            to="/Week"
-            className="flex items-center text-white transition-all duration-300 hover:translate-x-2"
-            onClick={() => handleClick(5, 1)}
-          >
-            <span className="hidden sm:block">Week 6</span>
-          </Link>
-        </li>
-        <li>
-          <Link
-            to="/Week"
-            className="flex items-center text-white transition-all duration-300 hover:translate-x-2"
-            onClick={() => handleClick(6, 1)}
-          >
-            <span className="hidden sm:block">Week 7</span>
-          </Link>
-        </li>{" "}
-        <li>
-          <Link
-            to="/Week"
-            className="flex items-center text-white transition-all duration-300 hover:translate-x-2"
-            onClick={() => handleClick(7, 1)}
-          >
-            <span className="hidden sm:block">Week 8</span>
-          </Link>
-        </li>{" "}
-        <li>
-          <Link
-            to="/Week"
-            className="flex items-center text-white transition-all duration-300 hover:translate-x-2"
-            onClick={() => handleClick(8, 1)}
-          >
-            <span className="hidden sm:block">Week 9</span>
-          </Link>
-        </li>
+
+        {pattern.map((item) => {
+          return (
+            <li>
+              <Link
+                to="/Week"
+                className="flex items-center text-white transition-all duration-300 hover:translate-x-2"
+                onClick={() => handleClick(item[0], item[1])}
+              >
+                <span className="hidden sm:block">Week {item[0] + 1}</span>
+              </Link>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
