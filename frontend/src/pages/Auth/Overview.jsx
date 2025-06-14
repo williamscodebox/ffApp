4229a92bb51100058f7cd78427cbe4a874abd2b5
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-06-14 14:56:57 -0400

    updated Overview

--- frontend/src/pages/Auth/Overview.jsx
+++ frontend/src/pages/Auth/Overview.jsx
@@ -1,14 +1,16 @@
 import Weeklies from "./Weeklies";
 import { week } from "../../../data/data";
+import { useValues } from "../../providers/ValueContext";
 
 const Overview = () => {
+  const { values } = useValues();
+
   return (
     <div className="mb-5 picks-bg">
       <div className="pt-5 pb-4">
         <div className="border-2 pt-5 mr-20 ml-20 pb-5 bg-gray-700 rounded-2xl text-white font-normal opacity-90">
           <p className="text-3xl ml-20">
-            Games for the week of {week[0][0] + " " + week[0][1]} -{" "}
-            {week[0][0] + " " + week[0][7]}
+            Games for the week of {values[0] + " " + values[6]}
           </p>
 
           <Weeklies />
