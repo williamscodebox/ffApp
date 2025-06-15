bd23d880d279e492247df3398e0f5c1d3ff7a393
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-06-15 14:14:54 -0400

    created selectionsRoutes

---
+++ backend/routes/selectionsRoutes.js
@@ -0,0 +1,22 @@
+import express from "express";
+
+// controllers
+import {} from "../controllers/selectionsController.js";
+
+// middlewares
+import { authenticate, authorizeAdmin } from "../middlewares/authMiddleware.js";
+
+const router = express.Router();
+
+router.route("/selections").post(createSelections);
+// // .get(authenticate, authorizeAdmin, getAllUsers);
+
+// router.post("/auth", loginUser);
+// router.post("/logout", logoutCurrentUser);
+
+// router
+//   .route("/profile")
+//   // .get(authenticate, getCurrentUserProfile)
+//   .put(authenticate, updateCurrentUserProfile);
+
+export default router;
