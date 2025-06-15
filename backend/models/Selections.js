e35879ac54275cfcd87aa22ddabf2b07d6151125
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-06-15 13:41:14 -0400

    added Selections Schema

---
+++ backend/models/Selections.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const selectionSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    }, // Links to User schema
+    selections: [
+      {
+        gameKey: { type: String, required: true }, // Unique identifier (date + time)
+        team: { type: String, required: true },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const Selection = mongoose.model("Selection", selectionSchema);
+export default Selection;
--- backend/models/User.js
+++ backend/models/User.js
@@ -2,6 +2,8 @@ import mongoose from "mongoose";
 
 const userSchema = mongoose.Schema(
   {
+    // userId: { type: String, required: true, unique: true }, // Unique user ID (e.g., Firebase UID or JWT ID)
+
     username: {
       type: String,
       required: true,
