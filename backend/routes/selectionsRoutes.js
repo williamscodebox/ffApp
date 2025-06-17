fad604f0b954b7167125cefa1da5b39c93142dd4
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-06-17 09:18:18 -0400

    updated selectionsRoutes

--- backend/routes/selectionsRoutes.js
+++ backend/routes/selectionsRoutes.js
@@ -18,12 +18,4 @@ router.route("/:userId/:week").get(authenticate, fetchSelections);
 router.route("/:userId/:week").put(authenticate, updateSelections);
 router.route("/:userId/:week").delete(authenticate, deleteSelections);
 
-// router.post("/auth", loginUser);
-// router.post("/logout", logoutCurrentUser);
-
-// router
-//   .route("/profile")
-//   // .get(authenticate, getCurrentUserProfile)
-//   .put(authenticate, updateCurrentUserProfile);
-
 export default router;
