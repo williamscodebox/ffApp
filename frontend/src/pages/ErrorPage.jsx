185ddea0139832170905aa00e705793bde2bc748
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-06-17 09:05:56 -0400

    updated ErrorPage

--- frontend/src/pages/ErrorPage.jsx
+++ frontend/src/pages/ErrorPage.jsx
@@ -1,8 +1,5 @@
-import React from "react";
-import ScrollTriggered from "../components/ScrollTriggered";
 import ScrollCard from "../components/ScrollCard";
-import { useDispatch, useSelector } from "react-redux";
-import { makeWeek, schedule } from "../../data/data";
+import { schedule } from "../../data/data";
 import { useValues } from "../providers/ValueContext";
 
 const ErrorPage = () => {
@@ -16,29 +13,9 @@ const ErrorPage = () => {
     "Nov 5",
     "Nov 6",
   ];
-  // const check = week[3].map((item) => {
-  //   if (typeof item === "object") {
-  //     console.log("fuck YA");
-  //     console.log(item);
-  //     item.shift();
-  //     console.log(item);
-  //   }
-  // });
-
-  // const week1 = makeWeek(0, 0);
-  // console.log(week1);
-  // const week2 = makeWeek(1, 0);
-  // console.log(week2);
-  // const week4 = makeWeek(3, 0);
-  // console.log(week4);
-  // const week13 = makeWeek(12, 2);
-  // console.log(week13);
-
-  schedule;
 
   return (
     <div>
-      {" "}
       {schedule.map((item) => {
         console.log(JSON.stringify(item.date));
         console.log(JSON.stringify(value));
