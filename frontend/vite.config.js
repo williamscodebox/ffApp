771f8768815e552f0451477b0517d1bf6415810f
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-06-18 09:00:56 -0400

    added shadeCn table

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwinds from "@tailwindcss/vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,6 +10,11 @@ export default defineConfig({
     proxy: {
       "/api/": "http://localhost:3000",
       // "/uploads/": "http://localhost:3000",
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
