883d0eace39ba19bd467afba99ab98e5c96e7ccf
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-06-24 18:45:25 -0400

    added AdminSidebar

---
+++ frontend/src/components/AdminSidebar.jsx
@@ -0,0 +1,70 @@
+import { Link } from "react-router-dom";
+import { useValues } from "../providers/ValueContext";
+import { makeWeek } from "../../data/data";
+
+function AdminSidebar() {
+  const { updateValues, updateWeek } = useValues();
+
+  const handleClick = (w, m) => {
+    // w is for week and m is for month
+    const update = makeWeek(w, m);
+    console.log(update);
+    updateValues(update);
+    updateWeek(w);
+  };
+
+  const pattern = [
+    [0, 0],
+    [1, 0],
+    [2, 0],
+    [3, 0],
+    [4, 1],
+    [5, 1],
+    [6, 1],
+    [7, 1],
+    [8, 1],
+    [9, 2],
+    [10, 2],
+    [11, 2],
+    [12, 2],
+    [13, 3],
+    [14, 3],
+    [15, 3],
+    [16, 3],
+    // [17, 4] //schedule yet to be determined // check nfl schedule for updates
+  ];
+
+  return (
+    <div className=" bg-gray-900 text-white w-[10rem] h-[87%] p-4 pt-10 pb-8 fixed">
+      <div className="h-full overflow-auto scrollbar-hide sidebar pl-5 pb-5">
+        <h1 className="text-xl">Dashboard</h1>
+        <ul className="mt-6 space-y-4">
+          <li>
+            <Link
+              to="" // This should be the route to the admin home page
+              className="flex items-center text-white transition-all duration-300 hover:translate-x-2"
+            >
+              <span className="hidden sm:block">Home</span>
+            </Link>
+          </li>
+
+          {pattern.map((item, index) => {
+            return (
+              <li key={index}>
+                <Link
+                  to="" // This should be the route to the admin page for each week
+                  className="flex items-center text-white transition-all duration-300 hover:translate-x-2"
+                  onClick={() => handleClick(item[0], item[1])}
+                >
+                  <span className="hidden sm:block">Week {item[0] + 1}</span>
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default AdminSidebar;
--- frontend/src/pages/User/Admin/Admin.jsx
+++ frontend/src/pages/User/Admin/Admin.jsx
@@ -1,10 +1,22 @@
 import React from "react";
+import { useEffect } from "react";
+import Overview from "../../Auth/Overview";
+import AdminSidebar from "@/components/AdminSidebar";
 
 const Admin = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
   return (
-    <div className="pl-[3rem] flex justify-center gap-[5rem]">
-      <div className="mr-[4rem] mt-[7rem]">
-        <h1 className="text-2xl font-semibold mb-4">Admin</h1>
+    <div className="flex flex-row justify-between mt-10">
+      <div className="hidden lg:flex">
+        <AdminSidebar />
+      </div>
+      <div className="w-[100%] lg:w-[90%]">
+        <h1 className="flex justify-center text-2xl font-semibold mt-4">
+          Admin
+        </h1>
+        {/* <Overview /> */}
       </div>
     </div>
   );
