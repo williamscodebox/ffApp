cc941742abf0641ea34196c1f7c690377d445d04
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-06-26 09:29:46 -0400

    updated Results model

--- backend/models/Results.js
+++ backend/models/Results.js
@@ -2,15 +2,12 @@ import mongoose from "mongoose";
 
 const resultsSchema = new mongoose.Schema({
   week: { type: Number, required: true },
-  gameId: { type: String, required: true, unique: true }, // Unique identifier ${date}-${teamA}-${teamB}
-  homeTeam: { type: String, required: true },
-  awayTeam: { type: String, required: true },
-  winner: { type: String, enum: ["home", "away", "draw"], required: true },
-  score: {
-    home: { type: Number, required: true },
-    away: { type: Number, required: true },
-  },
-  datePlayed: { type: Date, required: true },
+  winners: [
+    {
+      gameKey: { type: String, required: true }, // Unique identifier ${date}-${teamA}-${teamB}
+      team: { type: String, required: true },
+    },
+  ],
 });
 const Results = mongoose.model("Results", resultsSchema);
 export default Results;
