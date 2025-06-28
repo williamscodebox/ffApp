4e8c77fe0fefd82f7b15a64bf808b0060681f314
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-06-28 08:38:01 -0400

    updated results api

--- frontend/src/redux/api/results.js
+++ frontend/src/redux/api/results.js
@@ -24,6 +24,28 @@ export const resultsApiSlice = apiSlice.injectEndpoints({
       // providesTags: (result, error, { userId, week }) =>
       //   result ? [{ type: "Selections", id: `${userId}-${week}` }] : [],
     }),
+    deleteResults: builder.mutation({
+      query: ({ week }) => ({
+        url: `${RESULTS_URL}/${week}`,
+        method: "DELETE",
+        // headers: {
+        //   Authorization: `Bearer ${localStorage.getItem("token")}`,
+        // },
+      }),
+    }),
+    updateResults: builder.mutation({
+      query: ({ week, results }) => ({
+        url: `${RESULTS_URL}/${week}`,
+        method: "PUT",
+        // headers: {
+        //   Authorization: `Bearer ${localStorage.getItem("token")}`,
+        // },
+        body: { results },
+      }),
+      invalidatesTags: (result, error, { week }) => [
+        { type: "Results", id: `${week}` },
+      ],
+    }),
   }),
 });
 
